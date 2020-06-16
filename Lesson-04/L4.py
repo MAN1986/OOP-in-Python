3cb09c2cdf8b24bbbdcc54535ca66c55a3b7dbbd
Muhammad Ahsan Naeem <mahsan.naeem@gmail.com>
2020-06-16 15:33:40 +0500

    Create L4.py

---
+++ Lesson-04/L4.py
@@ -0,0 +1,10 @@
+from myClasses import Student
+
+std1=Student('Anwar','Ali','MCT-UET-01') 
+std2=Student('Akbar','Khan','MCT-UET-02')
+std3=Student('Hamza','Akhtar','MCT-UET-03')
+
+std1.registerSubject('CP-II','MOM','MOM')
+
+help(std1 ) 
+
