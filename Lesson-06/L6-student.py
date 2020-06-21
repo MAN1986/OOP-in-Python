cda3be815c93ce9c661e8e5f7d5b7e9fc0b7cc2a
Muhammad Ahsan Naeem <mahsan.naeem@gmail.com>
2020-06-21 13:16:01 +0500

    Create L6-student.py

---
+++ Lesson-06/L6-student.py
@@ -0,0 +1,11 @@
+from myClasses import Student
+
+std1=Student('Anwar','Ali','MCT-UET-01') 
+std2=Student('Akbar','Khan','MCT-UET-02')
+std3=Student('Hamza','Akhtar','MCT-UET-03')
+
+print(Student.allStudents)
+
+
+
+
