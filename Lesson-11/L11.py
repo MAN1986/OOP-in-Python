8daf9926bd1032a3983c0bbb8f7bc8226b547946
Muhammad Ahsan Naeem <mahsan.naeem@gmail.com>
2020-07-09 10:04:22 +0500

    Create L11.py

---
+++ Lesson-11/L11.py
@@ -0,0 +1,18 @@
+from myClasses import Student
+std1=Student('Anwar','Ali','MCT-UET-01') 
+std2=Student('Akbar','Khan','MCT-UET-02')
+std3=Student('Hamza','Akhtar','MCT-UET-03')
+std4=Student('Faisal','Iqbal','MCT-UET-04')
+
+std1.registerSubject('CP-II','MOM')
+std2.registerSubject('Mech','ES','MOM')
+std3.registerSubject('CP-II','LA')
+std4.registerSubject('Mech','LA','MOM','ES')
+
+std1+std2
+std2-std1
+print(std1._groupMember)
+
+# Student._allStudents.sort()
+# print(Student._allStudents)
+# print(min(Student._allStudents))
