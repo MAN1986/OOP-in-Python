279d4fe7f28239b051a20c90b480eccc6f5a3629
Muhammad Ahsan Naeem <mahsan.naeem@gmail.com>
2020-07-30 23:49:43 +0500

    Create L20.py

---
+++ Lesson-20/L20.py
@@ -0,0 +1,48 @@
+from math import sqrt
+class Polygon:
+    def __init__(self,*vertices):
+        self.vertices=[Polygon.Point(v[0],v[1]) for v in vertices]
+    @property
+    def lines(self):
+        pass  #Retun list of all line segments of Polygon
+    @property
+    def area(self):
+        if(self.shape=='Complex'):
+            raise TypeError('Area of Complex Polygon is out of scope!')
+        else:
+            pass # Shoelace Algorithm
+    @property
+    def perimeter(self):
+        pass # Complete Perimiter Calculations
+    @property
+    def shape(self):
+        pass
+        # All cross Products
+        # if (all cross product>=0 or all cross products <=0):
+            #return 'Convex'
+        #else:
+            # chech inersections
+            #     if(any intersection)
+            #         return 'Complex'
+            # return 'Concave'
+        
+    def __len__(self):
+        return len(self.vertices)
+    def __iter__(self):
+        return (p for p in self.vertices)
+
+    class Point:
+        def __init__(self,x,y):
+            self.x=x
+            self.y=y
+        def dist(self,other):
+            return sqrt((self.x-other.x)**2+(self.y-other.y)**2)
+        def __repr__(self):
+            return f'({self.x},{self.y})'
+        
+    class LSeg:
+        def __init__(self,p1,p2):
+            self.p1=p1
+            self.p2=p2
+        def __len__(self):
+            return self.p1.dist(self.p2)
